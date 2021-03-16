96102afec5fda3d5c69f0af96cb43e616b15320b
Jennie Wang <jenniewang@jennies-mbp.myfiosgateway.com>
2021-03-15 21:46:29 -0400

    Jennie update

--- front-end/src/PasswordResetConfimation.js
+++ front-end/src/PasswordResetConfimation.js
@@ -7,7 +7,7 @@ const PasswordResetConfirmation = (props) => (
     <div>
         return(
             <article>    
-                <h1>Welcome</h1>
+                <h1>Welcome!</h1>
                 <p1>A reset link has been sent to your email address. Please check and use the link to reset your password </p1>
                 <p2>Please go back to Login Page to sign in! </p2>
 
