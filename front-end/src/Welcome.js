a541c7e2de4775bcd5295ff1243e7e541582806f
Carrie Ba <zb631@nyu.edu>
2021-03-16 10:25:40 +0800

    home page setup

--- front-end/src/Welcome.js
+++ front-end/src/Welcome.js
@@ -10,18 +10,17 @@ const Welcome = (props) => (
           <div>
             <p onClick={props.action}>Log In</p>
           </div>
-        ); 
   </div>
 )
 const Section = () => {
-    return (
+    return ( 
         <section>
             <Welcome heading="Welcome" 
                 paragraph="Congrats! You have successfully signed up! An email has been sent to you. Please check and verify your email address! Welcome to RunNErrand!" 
                 paragraph2 = "Now you can go back to Login!"
                 action={ () => { alert("See what I mean? Javascript is fun!") } }
             />
-        </section>
+        </section> 
     )
 }
 
