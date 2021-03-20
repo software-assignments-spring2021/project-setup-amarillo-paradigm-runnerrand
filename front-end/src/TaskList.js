588904a0cf0f1004f545d9ea26be1cc1a5d57a16
Jennie Wang <jenniewang@jennies-mbp.myfiosgateway.com>
2021-03-20 12:43:30 -0400

    updating task lists from Jennie and Carrie

--- front-end/src/TaskList.js
+++ front-end/src/TaskList.js
@@ -2,11 +2,35 @@ import './TaskList.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const  = TaskList () => {
-    return(
-      <div>
-        
+const TaskList =  () => {
+  return(
+    <div>
+    <h1>Task Lists</h1>
+
+      <div className = "taskTitle">
+            <h1>Task1</h1>
+            <p>Click for more details</p>
       </div>
-    )
+      <div className = "taskTitle">
+            <h1>Task2</h1>
+            <p>Click for more details</p>
+      </div>
+      <div className = "taskTitle">
+            <h1>Task3</h1>
+            <p>Click for more details</p>
+      </div>
+      <div className = "taskTitle">
+            <h1>Task4</h1>
+            <p>Click for more details</p>
+      </div>
+      <div className = "taskTitle">
+            <h1>Task5</h1>
+            <p>Click for more details</p>
+      </div>
+
+      </div>
+  )
 }
+    
+
 export default TaskList;
