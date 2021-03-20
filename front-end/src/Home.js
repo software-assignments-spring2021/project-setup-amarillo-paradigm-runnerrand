a5c74833224ca074e930608030e2066e6d9764a6
Carrie Ba <zb631@nyu.edu>
2021-03-20 23:33:31 +0800

    rename pictures

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return(
       <div>
-      <div className="searchBar">
-      <p1> Enter a location: </p1>
-       <input type="text" id="SearchBar" name="LocationSearch" placeholder="Enter a location"/>
-       <p></p>
-       <h1>What do you need help for ? </h1>
-       <input type="text" id="JobSearchBar" name="JobSearch" placeholder="Search for jobs"/>
-      </div>
+        <div className="searchBar">
+            <p></p>
+            <p1> Enter a location: </p1>
+            <input type="text" id="SearchBar" name="LocationSearch" placeholder="Enter a location"/>
+       
+            <p></p>
+            <h1>What do you need help for ? </h1>
+            <input type="text" id="JobSearchBar" name="JobSearch" placeholder="Search for jobs"/>
+
+            <p></p>
+        </div>
+        <div>
+            <h1>Search by Catogories: </h1>
+          
+        </div>
      </div>
     );
    }
