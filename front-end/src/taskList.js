21eec6dc5cce6d06a60e151d807cc672c42b4dc2
Jennie Wang <jenniewang@jennies-mbp.myfiosgateway.com>
2021-03-20 12:33:20 -0400

    adding taskList page

---
+++ front-end/src/taskList.js
@@ -0,0 +1,12 @@
+import './TaskList.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const  = TaskList () => {
+    return(
+      <div>
+        
+      </div>
+    )
+}
+export default TaskList;
