a9d38ba0f20fbc309cace387b4f11e1a2392415d
zijiahu <hhuzijia@gmail.com>
2021-03-20 10:12:53 -0400

    added a heading for AccountDetails

--- front-end/src/AccountDetails.js
+++ front-end/src/AccountDetails.js
@@ -11,44 +11,28 @@ const accountDetails = (props) => {
         zipCode : "null"
     }
     return(
-        <section class="accountDetails">
-            <ProfilePicture> </ProfilePicture>
-            <h4 id="username">Username: {accountInfo.userName}</h4>
-            <p></p>
-            <button onClick={handleProfilePhoto}>Edit Profile Picture</button>
-            <p></p>
-            <h4>First Name: {accountInfo.firstName}</h4>
-            <p></p>
-            <h4>Last Name: {accountInfo.lastName}</h4>
-            <p></p>
-            <h4>Email: {accountInfo.email}</h4>
-            <p></p>
-            <h4>Phone Number: {accountInfo.phoneNumber}</h4>
-            <p></p>
-            <h4>Zip Code: {accountInfo.zipCode}</h4>
-            <p></p>
-            <button onClick={handleEditDetails}>Edit Account Details</button>
-            <p></p>
+        <section>
+            <h1 className="title">Account Details</h1>
+            <hr></hr>
+            <section class="accountDetails">
+                <ProfilePicture> </ProfilePicture>
+                <h4 id="username">Username: {accountInfo.userName}</h4>
+                <button onClick={handleProfilePhoto}>Edit Profile Picture</button>
+                <p></p>
+                <h4>First Name: {accountInfo.firstName}</h4>
+                <p></p>
+                <h4>Last Name: {accountInfo.lastName}</h4>
+                <p></p>
+                <h4>Email: {accountInfo.email}</h4>
+                <p></p>
+                <h4>Phone Number: {accountInfo.phoneNumber}</h4>
+                <p></p>
+                <h4>Zip Code: {accountInfo.zipCode}</h4>
+                <p></p>
+                <button onClick={handleEditDetails}>Edit Account Details</button>
+                <p></p>
+            </section>
         </section>
-        // <section>
-        //     <ProfilePicture class="profilePic"> </ProfilePicture>
-        //     <p></p>
-        //     <button onClick={handleProfilePhoto}>Profile Photo</button>
-        //     <p></p>
-        //     <button onClick={handleFirstName}>First Name</button>
-        //     <p></p>
-        //     <button onClick={handleLastName}>Last Name</button>
-        //     <p></p>
-        //     <button onClick={handleEmail}>Email</button>
-        //     <p></p>
-        //     <button onClick={handlePhoneNumber}>Phone Number</button>
-        //     <p></p>
-        //     <button onClick={handleZipCode}>Zip Code</button>
-        //     <p></p>
-        //     <button onClick={handleZipCode}>Edit</button>
-        //     <p></p>
-        // </section>
-        
     )
 }
 
