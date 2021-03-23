2604399a34a4ac7818481737bf5d240a37e81b95
zijiahu <hhuzijia@gmail.com>
2021-03-23 11:46:36 -0400

    created some simple code to launch the express app

--- back-end/app.js
+++ back-end/app.js
@@ -2,5 +2,8 @@
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 // we will put some server logic here later...
+app.get("/", (req, res) => {
+  res.send("Hello!")
+})
 // export the express app we created to make it available to other modules
 module.exports = app
