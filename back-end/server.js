371be844d5ce35e18f99fb8b6daf051ace7e3b3b
jessicalry <rl3541@nyu.edu>
2021-03-31 23:14:43 +0800

    added some useless stuff

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+const app = require("./app")
 const server = require("./app") // load up the web server
-const port = 3000 // the port to listen to for incoming requests
+const port = 5000 // the port to listen to for incoming requests
+
 // call express's listen function to start listening to the port
 const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`)
@@ -12,3 +14,40 @@ const close = () => {
 module.exports = {
   close: close,
 }
+
+app.post('/post', (req, res, next) => {
+  const newTask = {
+    taskTitle: req.body.email,
+    task: req.body.password,
+    taskCategory: req.body.firstname,
+    taskAddress: req.body.lastname,
+    taskDueDate: req.body.role
+    taskBudget:
+    taskContact:
+    taskRemarks:
+      }
+  console.log(newUser);
+  User.findOne({ username: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({error: 'An account already exists with that email. Please use a different email.'})
+    } else {
+      User.create(newUser, function(err, user) {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({error: 'Error creating user. Please try again'});
+        } else {
+          console.log('user', user);
+          console.log('Successfully created user');
+          req.logIn(user, function(err) {
+            if (err) {
+              return res.status(500).json({error: 'Issue with Passport authentication'});
+            }
+            return res.json({success: 'Successfully created user'});
+          });
+        }
+      })
+    }
+  })
+
+}
+});
