06dd813088923e048942fd57720d906884e21ac0
jy2575 <jy2575@nyu.edu>
2021-04-03 17:21:13 -0400

    update review model

--- back-end/review.model.js
+++ back-end/review.model.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
-
-
-
-
+var reviewSchema = new mongoose.Schema({
+    
+    rating:{
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String
+    }
+})
 module.exports = mongoose.model("Review", ReviewSchema)
