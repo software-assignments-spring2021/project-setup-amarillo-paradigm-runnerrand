65081ef5318067b380982a7815bab6342a0adb44
Carrie Ba <zb631@nyu.edu>
2021-04-03 23:39:04 +0800

    solve conflict

--- back-end/app.js
+++ back-end/app.js
@@ -8,10 +8,10 @@ const multer = require("multer") // middleware to handle HTTP POST requests with
 const axios = require("axios") // middleware for making requests to APIs
 require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
 const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
-
+const mongoose = require('mongoose');
 
 // we will put some server logic here later...
-const User = require('./User')
+//const User = require('./User')
 
 //<script type="module" src="../front-end/src/Home.js"></script>
 
