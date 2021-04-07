1fa860f82ed02537e197bf7f0545d1bdebce5466
jy2575 <jy2575@nyu.edu>
2021-04-07 02:41:07 -0400

    test update

--- back-end/test/test2.js
+++ back-end/test/test2.js
@@ -8,14 +8,9 @@ chai.use(chaiHttp);
 
 // POST /save-restaurant
 describe('post-task test', () => {
-  it('should respond with JSON data = {data:{xxx,xxx,xxx,xxx}}', () => {
-    chai.request(app)
-      .post('/post-task')
-      .send({title:"1",task_id:"2"})
-      .end((err, res) => {
-        console.log("Response Body:", res.body);
-        assert.equal(res.body.data.taskTitle,"1");
-        assert.equal(res.body.data.taskID,"2");
-      });
+    it('should respond with JSON data = {data:{xxx,xxx,xxx,xxx}}', () => {
+      chai.request(app)
+        
+        });
+    });
   });
-});
