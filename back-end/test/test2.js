634ce4f142d8c331cc42cf114320ac605190130f
jy2575 <jy2575@nyu.edu>
2021-04-07 02:40:17 -0400

    initial test set up

---
+++ back-end/test/test2.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+
+const { assert } = chai;
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.use(chaiHttp);
+
+// POST /save-restaurant
+describe('post-task test', () => {
+  it('should respond with JSON data = {data:{xxx,xxx,xxx,xxx}}', () => {
+    chai.request(app)
+      .post('/post-task')
+      .send({title:"1",task_id:"2"})
+      .end((err, res) => {
+        console.log("Response Body:", res.body);
+        assert.equal(res.body.data.taskTitle,"1");
+        assert.equal(res.body.data.taskID,"2");
+      });
+  });
+});
