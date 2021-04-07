f0497d63eb44148abb449c77d4d525fb2736ddcd
jy2575 <jy2575@nyu.edu>
2021-04-07 02:41:25 -0400

    test update

--- back-end/test/test2.js
+++ back-end/test/test2.js
@@ -10,7 +10,12 @@ chai.use(chaiHttp);
 describe('post-task test', () => {
     it('should respond with JSON data = {data:{xxx,xxx,xxx,xxx}}', () => {
       chai.request(app)
-        
+        .post('/post-task')
+        .send({title:"1",task_id:"2"})
+        .end((err, res) => {
+          console.log("Response Body:", res.body);
+          assert.equal(res.body.data.taskTitle,"1");
+          assert.equal(res.body.data.taskID,"2");
         });
     });
   });
