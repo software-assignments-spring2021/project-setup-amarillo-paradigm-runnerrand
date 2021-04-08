365e70cec60a37af3610b8692486614a4ee174a8
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2021-04-09 00:11:44 +0800

    Add files via upload

--- front-end/src/Post.js
+++ front-end/src/Post.js
@@ -1,7 +1,8 @@
 import './Post.css';
 import './Home'
-import React, { Component } from "react";
+import React, { useState } from "react"
 import DropdownButton from 'react-bootstrap/DropdownButton'
+import Axios from 'axios'
 import Dropdown from 'react-bootstrap/Dropdown'
 import SplitButton from 'react-bootstrap/SplitButton'
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,107 +13,52 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
-const Post = () =>{
+function Post() {
+    const url ="http://localhost:3000/post-task"
+    const [data, setData] = useState({
+        title: "",
+        category: "",
+        campus: "",
+        address: "",
+        duedate: "",
+        budget: "",
+        details: ""
+
+    })
+
+    function submit(e){
+        e.preventDefault();
+        Axios.post(url, {
+
+        })
+            .then(res => {
+                console.log(res.data)
+            })
+    }
+
+    function handle(e){
+        const newdata={...data}
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
     return (
-        <section>
-            <button onClick={handleGoBack}> Go Back </button>
-            <h1 className="title">Post Tasks</h1>
-            <hr></hr>
-
-            <br></br>
-            <label for="Task Title" className="item">Task Title:</label><br></br>
-            <input type="text" id="tasktitle" placeholder='' /> <br></br>
-
-            <br></br>
-            <label for="Category" className="item">Category:</label><br></br>
-           
-            <DropdownButton id="drpdwn" title="select a task categoty">
-            <Dropdown.Item href="#/action-1">Tutoring</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Assembly</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">......</Dropdown.Item>
-            </DropdownButton>
-           
-            <br></br>
-            
-
-            <label for="Category" className="item">Campus:</label><br></br>
-           
-            <DropdownButton id="drpdwn" title="select a campus">
-            <Dropdown.Item href="#/action-1">Manhattan</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Brooklyn</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">......</Dropdown.Item>
-            </DropdownButton>
-            <br></br>
-
-            <form action="/upload-task" method="POST" enctype="multipart/form-data">
-                {/* <input name="my_files" type="file" multiple />
-                <input type="submit" value="Submit!!!" /> */}
-
-                <label for="Address" className="item">Address:</label><br></br>
-                <input name="my_task" type="text" id="textbar"/> <br></br>
-
-                <br></br>
-                <label for="Task Title" className="item">Due Date:</label><br></br>
-                <input name="my_task" type="text" id="textbar"/> <br></br>
-
-                <br></br>
-                <label for="Task Title" className="item">Budget:</label><br></br>
-                <input name="my_task" type="text" id="textbar"/> <br></br>
-
-                <br></br>
-                <label for="Task Title" className="item">Contact:</label><br></br>
-                <input name="my_task" type="text" id="textbar"/> <br></br>
-
-                <br></br>
-                <label for="details" className="item">Details:</label><br></br>
-                {/* <textarea type="text" id="textarea"/> <br></br> */}
-                <input name="my_task" type="text" id="textbar"/> <br></br>
-
-                <input type="submit" value="Post" />
-            </form>
-           
-            {/* <label for="Adsress" className="item">Address:</label><br></br>
-            <input type="text" id="textbar"/> <br></br>
-
-            <br></br>
-            <label for="Task Title" className="item">Due Date:</label><br></br>
-            <input type="text" id="textbar"/> <br></br>
-
-            <br></br>
-            <label for="Task Title" className="item">Budget:</label><br></br>
-            <input type="text" id="textbar"/> <br></br>
-
-            <br></br>
-            <label for="Task Title" className="item">Contact:</label><br></br>
-            <input type="text" id="textbar"/> <br></br>
-
-            <br></br>
-            <label for="details" className="item">Details:</label><br></br>
-            <textarea type="text" id="textarea"/> <br></br> */}
-
-            {/* <div>
-                <button onClick={handlePostButton}>Post</button> 
-                <button onClick={handleCancelButtion}>Cancel</button>
-            </div> */}
-            
-
-        </section>
-    )
+        <div>
+            <div>
+                
+                <form onSubmit={(e)=> submit(e)}>
+                    <input onChange={(e) => handle(e)} id="title" value={data.title} placeholder="title" type="text"></input>
+                    <input onChange={(e) => handle(e)} id="category" value={data.category} placeholder="category" type="text"></input>
+                    <input onChange={(e) => handle(e)} id="campus" value={data.campus} placeholder="campus" type="text"></input>
+                    <input onChange={(e) => handle(e)} id="address" value={data.address} placeholder="address" type="text"></input>
+                    <input onChange={(e) => handle(e)} id="duedate" value={data.duedate} placeholder="duedate" type="date"></input>
+                    <input onChange={(e) => handle(e)} id="budget" value={data.budget} placeholder="budget" type="text"></input>
+                    <input onChange={(e) => handle(e)} id="details" value={data.details} placeholder="details" type="text"></input>
+                    <button>Submit</button>
+                </form>
+            </div>
+        </div>
+    );
 }
-
-const handleGoBack =() =>{
-    window.location.href = '../'
-}
-
-const handlePostButton = ()=>{
-    window.location.href = '../PostConfirmation';
-
-}
-const handleCancelButtion = ()=>{
-    window.location.href = '../Home';
-}
-
-
-
 
 export default Post
