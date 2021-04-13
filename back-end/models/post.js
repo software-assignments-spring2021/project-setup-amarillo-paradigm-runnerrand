8dd67d1a1a732fac5542e7ffd5de0f30abe895c0
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2021-04-14 01:32:41 +0800

    Add files via upload

---
+++ back-end/models/post.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    author:{
+        type: Schema.Types.ObjectId, ref: "User",
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    campus:{
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    duedate: {
+        type: Date, 
+        default: Date.now
+    },
+    budget:{
+        type: Number, 
+        required: true
+    },
+    details:{
+        type:String,
+        required: true
+    }
+})
+
+const Post = mongoose.model('post', postSchema)
+
+module.exports = Post
