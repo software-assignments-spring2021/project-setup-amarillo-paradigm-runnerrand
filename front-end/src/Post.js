69c2beee9730ffa125c652e76a7a790358ee2799
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2021-04-14 01:42:46 +0800

    Update Post.js

--- front-end/src/Post.js
+++ front-end/src/Post.js
@@ -14,7 +14,8 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 function Post() {
-    const url ="http://localhost:3000/post-task"
+    // const url ="http://localhost:3000/post-task"
+    const url ="http://localhost:3000/posts/create"
     const [data, setData] = useState({
         title: "",
         category: "",
@@ -28,12 +29,24 @@ function Post() {
 
     function submit(e){
         e.preventDefault();
-        Axios.post(url, {
+        // Axios.post(url, {
 
+        // })
+        //     .then(res => {
+        //         console.log(res.data)
+        //     })
+        const token = localStorage.getItem("token")
+        Axios({
+            method:'post',
+            url,
+            data,
+            headers:{
+                Authorization:token
+            }
+        }).then(res => {
+            console.log(res.data)
+            window.location.href = "/postsuccess"
         })
-            .then(res => {
-                console.log(res.data)
-            })
     }
 
     function handle(e){
@@ -43,15 +56,24 @@ function Post() {
         console.log(newdata)
     }
     return (
-        <div>
-            <div>
-                <form onSubmit={(e)=> submit(e)} className="postTask">
+        <React.Fragment>
+                <form onSubmit={submit} className="postTask">
                     <p id="postTitle">Posting a Task:</p>
                     <input onChange={(e) => handle(e)} id="title" value={data.title} placeholder="title" type="text" className="taskDetails"></input>
                     <p></p>
-                    <input onChange={(e) => handle(e)} id="category" value={data.category} placeholder="category" type="text" className="taskDetails"></input>
+                    <select name="" id="category" className="taskDetails" onChange={(e) => handle(e)} required >
+                        <option value="" disabled selected hidden>Category</option>
+                        <option value="Shopping">Shopping</option>
+                        <option value="Tutoring">Tutoring</option>
+                        <option value="Delivery">Delivery</option>
+                    </select>
                     <p></p>
-                    <input onChange={(e) => handle(e)} id="campus" value={data.campus} placeholder="campus" type="text" className="taskDetails"></input>
+                    <select name="" onChange={(e) => handle(e)} id="campus" className="taskDetails" required>
+                        <option value="" disabled selected hidden>Campus</option>
+                        <option value="Brooklyn">Brooklyn</option>
+                        <option value="Greenwich Village">Greenwich Village</option>
+                    </select>
+
                     <p></p>
                     <input onChange={(e) => handle(e)} id="address" value={data.address} placeholder="address" type="text" className="taskDetails"></input>
                     <p></p>
@@ -63,8 +85,7 @@ function Post() {
                     <p></p>
                     <button id="post">Submit</button>
                 </form>
-            </div>
-        </div>
+        </React.Fragment>
     );
 }
 
