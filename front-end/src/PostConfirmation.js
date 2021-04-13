ecfb3da9636df2a99df3fc9bbb4a6aad07949d10
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2021-04-14 01:43:14 +0800

    Update PostConfirmation.js

--- front-end/src/PostConfirmation.js
+++ front-end/src/PostConfirmation.js
@@ -7,7 +7,7 @@ const PostConfirmation = (props) => {
     return(
         <article>    
             <h1>Your task has been posted!</h1>
-            <p1>You will be notified when others place their bids.</p1>
+            <p>You will be notified when others place their bids.</p>
             <br></br>
             <p2></p2>
         </article>
