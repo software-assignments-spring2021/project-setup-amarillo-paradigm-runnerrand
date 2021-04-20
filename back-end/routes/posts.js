718d1c07bdc487ede9e55ff53893d4f0fe0c43af
jy2575 <jy2575@nyu.edu>
2021-04-20 10:03:33 -0400

    create function to retreive posts under specific user

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -4,6 +4,8 @@ const passport = require('passport')
 // const passportConf = require('../passport')
 const { validateBody, schemas } = require('../helpers/routehelpers')
 const PostsController = require('../controllers/posts')
+const posts = require('../controllers/posts')
+const Post = require('../models/post')
 
 
 router.route('/create')
@@ -12,7 +14,20 @@ router.route('/create')
 router.route('/auth_user')
     .get(passport.authenticate('jwt', { session: false}), PostsController.get_auth_user_posts)
 
+//get all existing posts
 router.route('/get')
     .get(PostsController.show_posts)
+
+//get all listings under specific user
+router.route("/user/:id").get(async (req, res) => {
+   try {
+    let findPosts = await Post.find({ author: req.params.id });
+    res.json(findPosts);
+       
+   } catch (error) {
+    res.status(500).json({ message: "error: find all listings under specific user" });
+   }
+    
+  });
 
 module.exports = router
