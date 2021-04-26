bf72c8fb40588ebd12508c99afbbec2ec5068881
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2021-04-26 20:10:35 +0800

    Add files via upload

--- front-end/src/TaskBrowser.js
+++ front-end/src/TaskBrowser.js
@@ -9,7 +9,7 @@ const TaskBrowser = (props) => {
 
   useEffect(() => {
 
-    axios.get(`http://localhost:3000/mytasks/${props.taskId}`)
+    axios.get(`${process.env.REACT_APP_BACKEND}/posts/${props.taskId}/get`)
       .then((response) => {
         console.log(response.data);
         setData(response.data);
@@ -26,6 +26,28 @@ const TaskBrowser = (props) => {
       })
     }, [props.taskId])
 
+  const handleComplete = () => {
+    const token = localStorage.getItem("token")
+    axios({
+        method:'post',
+        url:`${process.env.REACT_APP_BACKEND}/posts/${props.taskId}/pending-approval`,
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":token
+        },
+        data:{
+            id:data._id
+        }
+    })
+    .then((response) => {
+      console.log(response.data);
+      setData(prev => ({
+          ...prev,
+          status:"pending-approval"
+      }));
+    })
+  }  
+
   const imgSrc = `https://picsum.photos/200?id=${props.taskId}`;
 
   const routerContext = useContext(__RouterContext);
@@ -35,17 +57,18 @@ const TaskBrowser = (props) => {
     <div className="TaskList">
 
       <section className="main-content">
-        <article className="task" key={data.id}>
+        <article className="task" key={data._id}>
       
           <div className="details">
-            <id className="id">Task ID: {data.id}</id> 
+            <span className="id">Task ID: {data._id}</span> 
             <address className="address">Campus: {data.campus}</address>
             <p>Title: {data.title}</p>
-            <strong className="price">Price: {data.price}</strong><p>
+            <strong className="price">Price: {data.budget}</strong><p>
             <strong className="status">Status: {data.status}</strong></p>
-            <p>Description: {data.description}</p>
+            <p>Description: {data.details}</p>
             <p>Contact: {data.contact}</p>
             <button className="go-back" onClick={() => routerContext.history.goBack()}>Go Back</button>
+            { data.status === "accepted" && <button onClick={handleComplete}>Mark as Complete</button>}
           </div>
         </article>
       </section>
