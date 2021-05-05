3871be8f8b4e4b2f5623d3b8a03c485e66777b3b
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2021-05-06 00:41:11 +0800

    Add files via upload

--- back-end/test/test.js
+++ back-end/test/test.js
@@ -1,49 +1,11 @@
-var expect = require('chai').expect;
-var chai = require('chai'), chaiHttp = require('chai-http');
-var expect = chai.expect;
-const app = require("../app");
-chai.use(chaiHttp);
-const should = chai.should();
-const chaiHttp = require("chai-http");
-const assert = chai.assert;
+const app = require('../app') // Link to your server file
+const supertest = require('supertest')
+const request = supertest(app)
 
-const mongoose = require('mongoose');
-const Task = mongoose.model('Task');
-const User = mongoose.model('User');
+it('gets the test endpoint', async done => {
+  const response = await request.get('/test')
 
-const express = require('express');
-const app = express();
-require("../app.js");
-
-
-
-
-describe('User Model', function() {
-	describe('#save()', function() {
-
-		it('should not save a new User without a Username or Password created', function(done) {
-				const user = new User({
-					email: "user999@gmail.com",
-					tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
-				});
-      				user.save(function(err) {
-	        			expect(err).to.exist
-          				.and.be.instanceof(Error)
-        				done();
-      			});
-		});
-
-		it('should not save a new User without an email created', function(done) {
-				const user = new User({
-  					username: "user999",
-					password: "abc12345678",
-					tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
-				});
-      				user.save(function(err) {
-	        			expect(err).to.exist
-          				.and.be.instanceof(Error)
-        				done();
-      			});
-		});
-	});
-});
+  expect(response.status).toBe(200)
+  expect(response.body.message).toBe('Alive')
+  done()
+})
